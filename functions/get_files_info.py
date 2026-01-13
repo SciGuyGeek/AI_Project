5b7427cde734139d5ff98070dd1518e7c84a56bf
SciGuyGeek <david.a.digiovanni@gmail.com>
2026-01-13 13:55:42 -0500

    Final commit of AI tool

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,8 +1,8 @@
 import os
 from google.genai import types
 
-schema_get_file_info = types.FunctionDeclaration(
-            name="get_file_info",
+schema_get_files_info = types.FunctionDeclaration(
+            name="get_files_info",
             description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
             parameters=types.Schema(
                 type=types.Type.OBJECT,
@@ -15,7 +15,7 @@ schema_get_file_info = types.FunctionDeclaration(
             )
         )
 
-def get_file_info(working_directory, directory="."):
+def get_files_info(working_directory, directory="."):
     try:
         abs_path = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(abs_path, directory))
--- main.py
+++ main.py
@@ -22,33 +22,42 @@ def main():
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(
-        model=model_name,
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions],
-                                           system_instruction=system_prompt,
-                                           temperature=0),
-    )
-    if response.usage_metadata == None:
-        raise RuntimeError("No response found")
-    else:
-        if response.function_calls:
-            for ii in response.function_calls:
-                function_call_result = call_function(ii)
-                if function_call_result.parts == []:
-                    raise Exception("parts list is empty")
-                if function_call_result.parts[0].function_response.response == None:
-                    raise Exception("Response is empty")
-                results.append(function_call_result.parts[0])
+    for ii in range(20):
+        results = []
+        response = client.models.generate_content(
+            model=model_name,
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions],
+                                            system_instruction=system_prompt,
+                                            temperature=0),
+        )
+        if response.candidates != []:
+                for jj in response.candidates:
+                    messages.append(jj.content)
+        if response.usage_metadata == None:
+            raise RuntimeError("No response found")
         else:
-            print(response.text)
-    if args.verbose:
-        X = response.usage_metadata.prompt_token_count
-        Y = response.usage_metadata.candidates_token_count
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {X}\nResponse tokens: {Y}")
-        print(f"-> {function_call_result.parts[0].function_response.response}")
-
+            if response.function_calls:
+                for kk in response.function_calls:
+                    function_call_result = call_function(kk)
+                    if function_call_result.parts == []:
+                        raise Exception("parts list is empty")
+                    if function_call_result.parts[0].function_response.response == None:
+                        raise Exception("Response is empty")
+                    results.append(function_call_result.parts[0])
+                messages.append(types.Content(role="user", parts=results))
+            else:
+                print(response.text)
+                break
+        if args.verbose:
+            X = response.usage_metadata.prompt_token_count
+            Y = response.usage_metadata.candidates_token_count
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {X}\nResponse tokens: {Y}")
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        if ii == 19:
+            print("Maximum iterations reached")
+            exit(1)
 
 if __name__ == "__main__":
     main()
